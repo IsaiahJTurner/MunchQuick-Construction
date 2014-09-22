65d5c8794e4f9d656de4601fc49130d4e6d9e34c
Isaiah J Turner <github.com@isaiahjturner.com>
2014-09-22 18:03:50 -0400

    Initial Commit

---
+++ js/custom.js
@@ -0,0 +1,52 @@
+jQuery(document).ready(function($) {
+    'use strict';
+    $(window).load(function() {
+        $("#preload-content").delay(300).fadeOut("400");
+        $("#preload").delay(800).fadeOut("400");
+    });
+    $('input, textarea').placeholder();
+    $('#clock').countdown('2014/9/28', function(event) {
+        $(this).html(event.strftime('' +
+            '<div class="clock-section"><span>%D</span> <p>days</p></div> ' +
+            '<div class="clock-section"><span>%H</span> <p>hours</p></div> ' +
+            '<div class="clock-section"><span>%M</span> <p>minutes</p></div> ' +
+            '<div class="clock-section"><span>%S</span> <p>seconds</p></div>'
+        ));
+    });
+    $.backstretch(['img/background-01.jpg']);
+    $('#contactform').submit(function() {
+        var action = $(this).attr('action');
+        $("#message").fadeIn("100", function() {
+            $('#message').hide();
+            $.post(action, {
+                email: $('#email').val()
+            }, function(data) {
+                document.getElementById(
+                        'message').innerHTML =
+                    data;
+                $('#message').fadeIn("100");
+            });
+        });
+        return false;
+    });
+    $('#modal-open').on('click', function(e) {
+        var mainInner = $('#modal-window .container'),
+            modal = $('#modal');
+        mainInner.animate({
+            opacity: 0
+        }, 300, function() {
+            $('html,body').scrollTop(0);
+            modal.addClass('modal-active').fadeIn(600);
+        });
+        e.preventDefault();
+        $('#modal-close').on('click', function(e) {
+            modal.removeClass('modal-active').fadeOut(
+                300, function() {
+                    mainInner.animate({
+                        opacity: 1
+                    }, 200);
+                });
+            e.preventDefault();
+        });
+    });
+});
